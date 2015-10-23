49f9cedd4cd88c475f5ce834414bf6622ed34617
mbunge <marco.bunge@web.de>
2015-10-23 23:09:11 +0200

    add locator test

--- tests/LocatorTest.php
+++ tests/LocatorTest.php
@@ -9,7 +9,36 @@
 namespace Blast\Tests;
 
 
+use Blast\Config\Locator;
+use Puli\Repository\FilesystemRepository;
+use Puli\Repository\Api\Resource\FilesystemResource;
+
 class LocatorTest extends \PHPUnit_Framework_TestCase
 {
+
+    private $repository;
+
+    public function testLocateResource()
+    {
+        $locator = new Locator($this->repository);
+        $resource = $locator->locate('/config/config.php');
+
+        $this->assertInstanceOf(FilesystemResource::class, $resource);
+
+    }
+
+    public function testInitiation()
+    {
+        $locator = new Locator($this->repository);
+        $this->assertInstanceOf(FilesystemRepository::class, $locator->getRepository());
+    }
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->repository = new FilesystemRepository(__DIR__ . '/res');
+    }
+
 
 }
---
+++ tests/res/config/config.php
@@ -0,0 +1,15 @@
+<?php
+/*
+*
+* (c) Marco Bunge <marco_bunge@web.de>
+*
+* For the full copyright and license information, please view the LICENSE.txt
+* file that was distributed with this source code.
+*
+* Date: 23.10.2015
+* Time: 22:58
+*/
+
+return [
+    'test' => 'value'
+];
