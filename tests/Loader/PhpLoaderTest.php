3314d67e2cf132ce25d6f226c8d0d1f2394b2682
Marco Bunge <marco_bunge@web.de>
2015-10-26 11:35:37 +0100

    Update PhpLoaderTest.php

--- tests/Loader/PhpLoaderTest.php
+++ tests/Loader/PhpLoaderTest.php
@@ -16,7 +16,7 @@ use Puli\Repository\FilesystemRepository;
 
 class PhpLoaderTest extends \PHPUnit_Framework_TestCase
 {
-    public function testLocatePhpConfig()
+    public function testPhpConfig()
     {
         $resourceBasePath = dirname(__DIR__) . '/res';
         $repository = new FilesystemRepository($resourceBasePath);
@@ -28,17 +28,9 @@ class PhpLoaderTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($loader->validateExtension($resource));
         $this->assertFileExists($resource->getFilesystemPath());
         $config = require $resource->getFilesystemPath();
-        var_dump($loader->validateConfig($config));
-        var_dump($loader->validateExtension($resource));
-        var_dump($config);
-
         $this->assertTrue($loader->validateConfig($config));
-        
-        var_dump($loader->transform($resource));
-        var_dump($loader->load($resource));
-        
-        //$this->assertInternalType('array', $loader->transform($resource));
-        //$this->assertInternalType('array', $loader->load($resource));
+        $this->assertInternalType('array', $loader->transform($resource));
+        $this->assertInternalType('array', $loader->load($resource));
     }
 
 }
